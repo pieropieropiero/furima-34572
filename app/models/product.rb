f197056ad71b30695371494d85fc65931b97cbf4
pieropieropiero <moonwalkerpierrot@gmail.com>
2021-03-03 20:35:51 +0900

    ActiveHashを導入する

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,23 @@
 class Product < ApplicationRecord
 
+  validates :name, length: {maximum: 40}, presence: true
+  validates :introduction, length: {maximum: 1000}, presence: true
+
+  validates :category_id, :status_id, :sent_fee_id, :sent_area_id, :arrival_period_id, :price, :image, presence: true
+  validates :category_id, :status_id, :sent_fee_id, :sent_area_id, :arrival_period_id, numericality: { other_than: 1 }
+  
+  validates :price, numericality: { only_integer: true }, inclusion: { in: 300..9999999 }
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :sent_fee
+  belongs_to_active_hash :sent_area
+  belongs_to_active_hash :arrival_period
+  
+  belongs_to :user
   has_one_attached :image
-
-
 
 
 end

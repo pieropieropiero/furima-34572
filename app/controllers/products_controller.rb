10d690bce0723f8962dc2511bf25eefe28279b3b
pieropieropiero <moonwalkerpierrot@gmail.com>
2021-03-04 12:03:22 +0900

    商品出品ページを表示できるようにする

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,29 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+  def product_params
+    params.require(:product).permit(:name, :introduction, :category_id, :status_id, :sent_fee_id, :sent_area_id, :arrival_period_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+
+
+
+
 end
--- app/models/sent_fee.rb
+++ app/models/sent_fee.rb
@@ -1,4 +1,4 @@
-class Sent_fee < ActiveHash::Base
+class SentFee < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
     { id: 2, name: '着払い(購入者負担)' },

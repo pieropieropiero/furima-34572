43edaf6bb10aef14bac02bda2be8bdeca78b7fc3
pieropieropiero <moonwalkerpierrot@gmail.com>
2021-03-07 20:12:27 +0900

    contributor_confirmationの追記

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:edit, :update]
   before_action :set_product, only: [:show,:edit, :update] #多分後でいる, :destroy
+  before_action :contributor_confirmation, only: [:edit, :update] #, :destroy
 
   def index
     @products = Product.all.order("created_at DESC")
@@ -43,6 +44,9 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @product.user
+  end
 
 
 end
